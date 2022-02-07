f65f2eb799eac22bcb9d0fd995755f52938304c6
Breval Le Floch <breval.lefloch@rca.fr>
2022-02-07 13:55:20 +0100

    change favicon

---
+++ .next/static/3ZrKQgpBsNHmbJB3a2XM_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5aa70ffc6d5888be.css","static/chunks/pages/index-a4d29f6dff567728.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- .next/static/wpeGyTvPunNMMxTp3aONr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5aa70ffc6d5888be.css","static/chunks/pages/index-7d35139c9d36dcdf.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
